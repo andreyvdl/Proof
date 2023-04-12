71cd2d7b9d5f5857efe29006c3f00e664a3510e8
adantas- <adantas-@student.42sp.org.br>
2023-04-12 17:52:13 -0300

    W:alpha_mirror wip

---
+++ RANK02/LEVEL2/alpha_mirror/alpha_mirror.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+#define ALPHA_LOW "zyxwvutsrqponmlkjihgfedcba"
+#define ALPHA_UPP "ZYXWVUTSRQPONMLKJIHGFEDCBA"
+
+int	main(int argcount, char **argverb)
+{
+	if (argcount == 2)
+		
+	write(STDOUT_FILENO, "\n", 1);
+	return (0);
+}
