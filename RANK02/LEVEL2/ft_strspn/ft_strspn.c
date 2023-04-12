1a321a155c6378a7294bccb6abe651ffc67d0bff
adantas- <adantas-@student.42sp.org.br>
2023-04-12 15:31:18 -0300

    M:fix

--- RANK02/LEVEL2/ft_strspn/ft_strspn.c
+++ RANK02/LEVEL2/ft_strspn/ft_strspn.c
@@ -2,7 +2,7 @@
 #define TRUE 1
 #define FALSE 0
 
-static int	is_part(char character, char *reference)
+static int	is_part(char character, const char *reference)
 {
 	size_t	index;
 
@@ -21,7 +21,7 @@ size_t	ft_strspn(const char *s, const char *accept)
 	size_t	size;
 
 	size = 0;
-	while (is_part(s[size], accept) == TRUE)
+	while (s[size] != '\0' && is_part(s[size], accept) == TRUE)
 		size++;
 	return (size);
 }
